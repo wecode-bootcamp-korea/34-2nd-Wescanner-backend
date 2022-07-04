4e9c47ee5acb42335febe64c12826d12cb430444
jeongjimin <wlals3968@naver.com>
2022-07-04 17:41:47 +0900

    CREATE : 모델링 구현
    CREATE : 모델링 구현
    
    UPDATE : 모델링 구현
    
    CREATE : 모델링 구현
    
    parent 74d5ed423f3fadeea5a135bd3089c3c3afb29539
    author jeongjimin <wlals3968@naver.com> 1656924107 +0900
    committer jeongjimin <wlals3968@naver.com> 1657005779 +0900
    
    UPDATE : 프로젝트 초기셋팅 수정
    
    slash 옵션 추가 완료
    
    model 구현 중
    
    CREATE : models.py
    
    users  -> 리뷰(사진,콘텐츠), wish_list
    hotels -> 예약이 이미 완료되어있는 hotel  테이블, hotel option 테이블 등 생성
    
    UPDATE : model 수정
    
    UPDATE : 모델링 수정
    
    UPDATE : 모델링수정
    
    kakao 소셜 로그인을 위해 고유 코드를 받을 kakao_id 컬럼 생성
    
    UPDATE : 모델링 수정
    
    City에 town 커럼 추가
    
    UPDATE : 모델링 수정
    
    hotel <-> hotelsite 다대다연결
    
    UPDATE : 모델링 수정
    
    hotel <-> hotelsite 다대다연결
    
    UPDATE : 모델링 구현
    
    다대다 연결 수정 및 최종 수정

---
+++ hotels/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+from core.models import TimeStampModel
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length = 50)
+    
+    class Meta:
+        db_table = 'countries'
+    
+class City(models.Model):
+    country = models.ForeignKey("Country",on_delete=models.CASCADE)
+    name    = models.CharField(max_length = 50)
+    town    = models.CharField(max_length = 50)
+    
+    class Meta:
+        db_table = 'cities'
+
+class Conformation(models.Model):
+    contents = models.CharField(max_length = 50)
+    
+    class Meta:
+        db_table = 'conformations'
+        
+class Hotel(models.Model):   
+    city         = models.ForeignKey("City",on_delete = models.CASCADE)
+    conformation = models.ForeignKey("Conformation",on_delete = models.CASCADE)
+    name         = models.CharField(max_length = 100)
+    rating       = models.IntegerField()
+    address      = models.CharField(max_length = 200)
+    
+    class Meta:
+        db_table = 'hotels'
+        
+class ImageUrl(models.Model):
+    hotel      = models.ForeignKey("Hotel",on_delete = models.CASCADE)
+    image_urls = models.URLField()
+    
+    class Meta:
+        db_table = 'image_urls'
+
+class Site(models.Model):   
+    name           = models.CharField(max_length = 50)
+    site_url       = models.URLField()
+    logo_image_url = models.URLField()
+    
+    class Meta:
+        db_table = 'sites'
+        
+class HotelSite(models.Model):   
+    hotel          = models.ForeignKey("Hotel",on_delete = models.CASCADE)
+    site           = models.ForeignKey("HotelSite",on_delete = models.CASCADE)
+    price          = models.DecimalField(max_digits = 10, decimal_places = 3)
+    is_free_cancel = models.BooleanField(default = False)
+    
+    class Meta:
+        db_table = 'hotels_sites'
+
+class Reserved(TimeStampModel):
+    hotel     = models.ForeignKey("Hotel",on_delete = models.CASCADE)
+    check_in  = models.DateField()
+    check_out = models.DateField()
+    
+    class Meta:
+        db_table = 'reserved'
