b151f49e49f334d412cee063004ea446afd3103b
jeongjimin <wlals3968@naver.com>
2022-07-04 17:41:47 +0900

    CREATE : 모델링 구현
    CREATE : 모델링 구현
    
    UPDATE : 모델링 구현
    
    CREATE : 모델링 구현
    
    parent 74d5ed423f3fadeea5a135bd3089c3c3afb29539
    author jeongjimin <wlals3968@naver.com> 1656924107 +0900
    committer jeongjimin <wlals3968@naver.com> 1657005779 +0900
    
    UPDATE : 프로젝트 초기셋팅 수정
    
    slash 옵션 추가 완료
    
    model 구현 중
    
    CREATE : models.py
    
    users  -> 리뷰(사진,콘텐츠), wish_list
    hotels -> 예약이 이미 완료되어있는 hotel  테이블, hotel option 테이블 등 생성
    
    UPDATE : model 수정
    
    UPDATE : 모델링 수정
    
    UPDATE : 모델링수정
    
    kakao 소셜 로그인을 위해 고유 코드를 받을 kakao_id 컬럼 생성
    
    UPDATE : 모델링 수정
    
    City에 town 커럼 추가
    
    UPDATE : 모델링 수정
    
    hotel <-> hotelsite 다대다연결
    
    UPDATE : 모델링 수정
    
    hotel <-> hotelsite 다대다연결
    
    UPDATE : 모델링 구현
    
    다대다 연결 수정 및 최종 수정

---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from core.models import TimeStampModel
+
+# Create your models here.
+class User(TimeStampModel):   
+    email    = models.CharField(max_length = 45,unique = True)
+    kakao_id = models.IntegerField(unique = True)
+    class Meta:
+        db_table = 'users'
+
+class Review(TimeStampModel):   
+    user     = models.ForeignKey("User",on_delete = models.CASCADE)
+    hotel    = models.ForeignKey("hotels.Hotel",on_delete = models.CASCADE)
+    contents = models.TextField()
+    rating   = models.IntegerField()
+    
+    class Meta:
+        db_table = 'reviews'
+
+class ReviewImage(TimeStampModel):   
+    review = models.ForeignKey("Review",on_delete = models.CASCADE)
+    url    = models.URLField()
+    
+    class Meta:
+        db_table = 'review_images'
+        
+class WishList(TimeStampModel):   
+    user   = models.ForeignKey("User",on_delete = models.CASCADE)
+    hotel  = models.ForeignKey("hotels.Hotel",on_delete = models.CASCADE)
+    
+    class Meta:
+        db_table = 'wish_lists'
