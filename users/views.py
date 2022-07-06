51e1a428553306c205aaa425bb3a3851e8132bc2
jeongjimin <wlals3968@naver.com>
2022-07-06 11:12:36 +0900

    CREATE : 이메일 로그인 구현
    CREATE : 이메일 로그인 구현
    
    이메일이 없으면 create 후 로그인
    존재하면 create없이 로그인
    
    UPDATE : 이메일 로그인
    
    변동사항없음

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+import re
+import jwt
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.views           import View
+from django.core.exceptions import ValidationError
+
+from wescanner.settings import SECRET_KEY,ALGORITHM
+from users.models   import User
+
+class EmailLoginView(View):
+    def post(self,request):
+        try: 
+            data     = json.loads(request.body)
+            email    = data['email']
+
+            REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
+
+            if not re.match(REGEX_EMAIL,email) :
+                return JsonResponse({"message":"INVALID_EMAIL"}, status=404)
+
+            if not User.objects.filter(email = email).exists():
+                User.objects.create(
+                    email = email
+                )
+
+            user = User.objects.get(email = email)
+
+            token  = jwt.encode({'user_id' : user.id}, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({"access_token" : token},status=200)
+
+        except KeyError: 
+            return JsonResponse({"message":"KEY_ERROR"},status=400)
