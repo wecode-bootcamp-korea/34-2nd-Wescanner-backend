e24a5658f59908a133a4a4488a1d70349836de73
jimin <trace_of_jimin@naver.com>
2022-07-13 17:48:56 +0900

    UPDATE : WIP
    CREATE : 호텔 리스트 리팩토리 완료
    
    CREATE : 호텔 리스트 리팩토리 완료
    
    CREATE : 호텔 리스트 리팩토링
    
    UPDATE :  호텔리스트 리팩토링
    
    UPDATE : 호텔 리팩토링
    
    UPDATE : 호텔 리팩토링
    
    UPDATE :  호텔리스트
    
    WIP

--- hotels/views.py
+++ hotels/views.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from django.http      import JsonResponse
 from django.views     import View
-from django.db.models import Q
+from django.db.models import Q, Min, Max
 
 from .models import (
     Country,
@@ -40,59 +40,32 @@ class HotelListView(View):
     def get(self, request):
         check_in        = request.GET.get("check_in")
         check_out       = request.GET.get("check_out")
-        price_min       = request.GET.get('price_min',0)
-        price_max       = request.GET.get('price_max',1000000)
-        country_id      = request.GET.get('country_id')
-        hotel_id        = request.GET.get('hotel_id')
-        city_id         = request.GET.get('city_id')
-        rating          = request.GET.get('rating')
-        is_free_cancel  = request.GET.get('is_free_cancel')
-        conformation_id = request.GET.get('conformation_id')
-        sort            = request.GET.get('sort','?')
-
-        q = Q()
-
-        if country_id:
-            q &= Q(city__country_id = country_id)
-            
-        if city_id:
-            q &= Q(city_id = city_id)
-
-        if hotel_id:
-            q &= Q(id = hotel_id)
-
-        if conformation_id :
-            q &= Q(conformation_id=conformation_id)
-
-        if rating :
-            q &= Q(rating=rating)
-            
-        p = Q()
-
-        if price_min and price_max :
-           p &= Q(price__range = (price_min,price_max))
+        sort            = request.GET.get('sort','id')
         
-        if price_max == '100000' :
-            p &=  Q(price__lte=100000)
-        
-        elif price_max == '200000' : 
-            p &=  Q(price__lte=200000)
-        
-        elif price_max == '400000' : 
-            p &=  Q(price__lte=400000)
-        
-        elif price_max == '600000' : 
-            p &=  Q(price__lte=600000)
-        
-        elif price_max == '800000' : 
-            p &=  Q(price__lte=800000)
+        FILTER_SET = {
+            'country_id' : 'city__country_id',
+            'city_id'    : 'city_id',
+            'rating'     : 'rating__in',
+            'hotel_id'   : 'id',
+            'conformation_id' : 'conformation_id__in',
+            'price_max'      :'hotelsite__price__lte',
+        }
 
-        elif price_max == '1000000' : 
-            p &=  Q(price__lte=1000000)
+        HOTEL_FILTER_SET = {
+            'price_max'      :'price__lte',
+            'is_free_cancel' : 'is_free_cancel'
+        }
 
-        if is_free_cancel :
-            p &= Q(is_free_cancel = is_free_cancel)
+        SORT_SET = {
+            'id'         : 'id',
+            'random'     : '?',
+            'low_price'  : 'hotelsite__price',
+            'high_price' : '-hotelsite__price'
+        }
         
+        q = {FILTER_SET.get(key) : json.loads(value) for key, value in request.GET.items() if key in FILTER_SET.keys()}
+        p = {HOTEL_FILTER_SET.get(key) : json.loads(value) for key, value in request.GET.items() if key in HOTEL_FILTER_SET.keys()}
+  
         a = Q()
 
         if check_in and check_out:
@@ -102,16 +75,8 @@ class HotelListView(View):
             a &= Q(reserved__check_in__range=(check_in, check_out))
             a &= Q(reserved__check_out__range=(check_in, check_out))
         
-        sort_set = {
-            'random'     : '?',
-            'low_price'  : 'hotelsite__price',
-            'high_price' : '-hotelsite__price'
-        } 
-        
-        order_field = sort_set.get(sort, 'id')
+        hotels = Hotel.objects.filter(**q).exclude(a).order_by(SORT_SET[sort])
 
-        hotels = Hotel.objects.filter(q).exclude(a).order_by(order_field)
-        
         result = [
             {
                 "hotels" : 
@@ -132,7 +97,6 @@ class HotelListView(View):
                             "image_url" : image_url.image_urls
                         } for image_url in hotel.imageurl_set.all()
                         ],
-
                         "conformation_id"       : hotel.conformation.id,
                         "conformation_contents" : hotel.conformation.contents,
                         "hotel_sites" : [
@@ -144,8 +108,7 @@ class HotelListView(View):
                                 "site_name"           : hotelsite.site.name,
                                 "site_url"            : hotelsite.site.site_url,
                                 "site_logo_image_url" : hotelsite.site.logo_image_url,
-                           
-                            } for hotelsite in hotel.hotelsite_set.filter(p)
+                            } for hotelsite in hotel.hotelsite_set.filter(**p)
                         ]
                     }
             } for hotel in hotels
